e2c9f69e628703b58e30e485ee365e283987decb
Onni Anttoora <onni.anttoora@gmail.com>
2020-10-14 14:19:02 +0300

    14.18 version with test text

--- script.js
+++ script.js
@@ -8,3 +8,24 @@ output.innerHTML = slider.value;
 slider.oninput = function() {
 output.innerHTML = this.value;
 }
+
+// test script for getting the answer to change based on slider
+var slider = document.getElementById("myRange"); // variable slider takes value from document with id myRange
+var output = document.getElementById("answer-value");
+output.innerHTML = slider.value;
+
+
+slider.oninput = function() {
+output.innerHTML = this.value;
+}
+
+// slider reactions test code
+function readTemperature(temp) {
+  if(temp <= 15) {
+  return("Cold");
+} else if(temp > 25) {
+  return("Hot");
+} else {
+return("Moderate");
+}
+}
